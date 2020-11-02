6920961f0d04942056fbc882e712562fe5133d9d
Natalya Apasova <natalya.gulak.apasova@yandex.by>
2020-11-02 14:47:11 +0300

    feat: Implement a program which reads a string from the standard input stdin, reverses it and then writes it to the standard output stdout. refactor: fix script and keywords in the package.json.

---
+++ src/nodejs_homework1/task1.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+
+const reverseString = function(string) {
+  return `${string.split('').reverse().join('')} \n`;
+}
+
+const lineReader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+lineReader.question('Enter a line:\n\n', (line) => {
+  console.log(reverseString(line));
+})
+
+lineReader.on('line', (line) => {
+  console.log(reverseString(line));
+})
