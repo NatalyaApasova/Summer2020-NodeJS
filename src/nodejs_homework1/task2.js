014b30548a8f3789bd0889c0df4ac7d5c59e31d5
Natalya Apasova <natalya.gulak.apasova@yandex.by>
2020-11-03 22:18:09 +0300

    feat: add the second task of the first node homework (create a program which converts csv to json and then writes it in a txt file)

---
+++ src/nodejs_homework1/task2.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const csv = require('csvtojson');
+const { pipeline } = require('stream');
+
+const csvFilePath = './src/nodejs_homework1/csv/nodejs-hw1-ex1.csv';
+const txtFilePath = './src/nodejs_homework1/nodejs-hw1-ex1.txt';
+
+const writeStream = fs.createWriteStream(txtFilePath);
+
+csv({
+  downstreamFormat: 'line',
+  headers: ['book', 'author', 'amount', 'price'],
+  colParser: {
+    "book": "string",
+    "author": "string",
+    "amount": "number",
+    "price": "number"
+  },
+  checkType: true
+})
+  .fromFile(csvFilePath)
+  .on('data', (buffer) => {
+    data = buffer.toString();
+    console.log(data);
+    pipeline(
+      data,
+      () => {
+        return new Promise((resolve,reject) => {
+          fs.appendFile(txtFilePath, data, (err) => {
+            if (err) {
+              console.error('Fail.', err)
+            }
+          }, console.log('Done.'))
+        })
+      },
+      (err) => {
+        if (err) {
+          console.error('Fail.', err)
+        }
+      }
+    );
+  })
+  .on('error', (err) => {
+    if (err) {
+      console.error('Fail.', err);
+    }
+  })
